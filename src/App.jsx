b435acad7c717d3706974e936e4f3744a046c12b
SwapnilPanchade <sspanchade113@gmail.com>
2024-06-27 10:42:25 +0530

    This is application for count and filter logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import { useContext, useMemo, useState } from "react"
+import { useContext, useMemo, useState } from "react";
 import { CountContext } from "./context";
-import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import {
+  RecoilRoot,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
 import { countAtom, evenSelector } from "./store/atoms/count";
 
 function App() {
@@ -10,49 +15,64 @@ function App() {
         <Count />
       </RecoilRoot>
     </div>
-  )
+  );
 }
 
 function Count() {
-  console.log("re-render");
-  return <div>
-    <CountRenderer />
-    <Buttons />
-  </div>
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons />
+    </div>
+  );
 }
 
 function CountRenderer() {
   const count = useRecoilValue(countAtom);
-  
-  return <div>
-    <b>
-      {count}
-    </b>
-    <EvenCountRenderer />
-  </div>
+
+  return (
+    <div>
+      <b>{count}</b>
+      <EvenCountRenderer />
+    </div>
+  );
 }
 
 function EvenCountRenderer() {
   const isEven = useRecoilValue(evenSelector);
 
-  return <div>
-    {isEven ? "It is even" : null}
-  </div>
+  return <div>{isEven ? "It is odd" : "this is even"}</div>;
 }
 
 function Buttons() {
   const setCount = useSetRecoilState(countAtom);
   console.log("buttons re-rendererd");
 
-  return <div>
-    <button onClick={() => {
-      setCount(count => count + 1)
-    }}>Increase</button>
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount((count) => count + 1);
+        }}
+      >
+        Increase
+      </button>
 
-    <button onClick={() => {
-      setCount(count => count - 1)
-    }}>Decrease</button>
-  </div>
+      <button
+        onClick={() => {
+          setCount((count) => count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+/**
+ * 2 input boxes (title, description) it also in an atom
+ * button (put the todo)
+ * filter(gym) [let's filter the todo], store this in an atom
+ */
